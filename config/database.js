f0877020b6ec7cc0857e5f5d7755bf773c57a4b4
TieuMinhGame <minhreuspro123@gmail.com>
2021-11-01 00:53:26 +0700

    commit12

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-	'url': 'mongodb+srv://cluster0.9h5fe.mongodb.net/server',
+	'url': 'mongodb+srv://admin:@123456@cluster0.9h5fe.mongodb.net/server?retryWrites=true&w=majority',
 	'options': {
-		'user':   'admin',
-		'pass':   '@123456',
+		//'user':   'admin',
+		//'pass':   '@123456',
 		'dbName': 'server',
 		'useNewUrlParser': true,
 		'useUnifiedTopology': true,
