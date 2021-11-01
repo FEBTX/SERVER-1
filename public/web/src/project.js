b4019c0306da5ddde736613c7cb0751553a6a270
TieuMinhGame <minhreuspro123@gmail.com>
2021-11-01 13:13:47 +0700

    commit1

--- public/web/src/project.js
+++ public/web/src/project.js
@@ -842,7 +842,7 @@ window.__require = function t(e, i, n) {
             },
             _onSocketError: function(t) {},
             reconnect: function() {
-                this.connect("m88cm.herokuapp.com", "/ws")
+                this.connect("m88cm.herokuapp.com", "/wss")
             },
             init: function() {
                 this.initPrototype()
